80c0535d702bed6b9abebe40bc50a2c4e799e4f3
Sebastian Di Luzio <sebidiluzio@gmail.com>
2018-06-21 18:21:58 +0200

    added explanation and changed code to english

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # funStuff
+This is a collection on some fun challenges or code ideas i've had over the course of time
+
+#abccba
+The premise of this challenge was to find a!=b!=c for which this applies:
+with abc not being multiplication but a decimal number with the length of 3 -> abc * 3 = cba
+
+The code is a brute force approach, which gives us the answer pretty quick. (O(10^3))
+If there were more possible inputs the code would've needed to be optimized.
--- abccba/src/main.java
+++ abccba/src/main.java
@@ -4,7 +4,7 @@ public class main {
 
 
     public static void main(String[] args) {
-
+        //print all solutions
         for (int[] l : losungen()
                 ) {
             System.out.println("a: " + l[0] + "b: " + l[1] + "c: " + l[2] + "\n");
@@ -12,20 +12,22 @@ public class main {
     }
 
     private static ArrayList<int[]> losungen() {
+        //create a list we want to save the solutions in
         ArrayList<int[]> out = new ArrayList<>();
+        //loop through all possible inputs
         for (int a = 0; a < 10; a++) {
             for (int b = 0; b < 10; b++) {
                 for (int c = 0; c < 10; c++) {
+                    //check if the formula is satisfied by the input
                     if (((a * 100 + b * 10 + c) * 3 == c * 100 + b * 10 + a) && a != b && b != c && c != a) {
+                        //if yes, add the values to the solution
                         int[] add = {a, b, c};
                         out.add(add);
                     }
                 }
             }
         }
-
-        System.out.println("\nEs gibt " + out.size() + " Lösungen.");
-
+        System.out.println("\nThere are " + out.size() + " Solutions.");
         return out;
     }
 }
